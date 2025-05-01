811aea59518ab0b359edaeee24b509f4ecc2909e
viggaskriver05 <viggaskriver@gmail.com>
2025-05-01 14:19:54 +0200

    stickman animation incorporation

--- Whiteboard Makker/Assets/Movement1.cs
+++ Whiteboard Makker/Assets/Movement1.cs
@@ -11,6 +11,7 @@ public class Movement1 : MonoBehaviour
     private bool moveRight;
     private bool jump;
 
+    public Animator animator;
     public LayerMask groundLayer;
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
@@ -28,22 +29,35 @@ public class Movement1 : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
             jump = true;
+
     }
 
     void FixedUpdate()
     {
         float moveDirection = 0f;
-        if (moveLeft) moveDirection -= 1f;
-        if (moveRight) moveDirection += 1f;
+        if (moveLeft)
+            moveDirection -= 1f;
 
-        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+        if (moveRight)
+            moveDirection += 1f;
+
+        animator.SetBool("isWalking", moveDirection != 0);
+        rb.linearVelocity = new Vector2(moveDirection * moveSpeed, rb.linearVelocity.y);
+
 
         if (jump && IsGrounded())
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            animator.SetBool("isJumping", true);
+
+        }
+        else
+        {
+            animator.SetBool("isJumping", false);
         }
 
         jump = false; // Reset jump after applying it
+
     }
 
     bool IsGrounded()
@@ -59,4 +73,5 @@ public class Movement1 : MonoBehaviour
     public void OnMoveRightUp() => moveRight = false;
 
     public void OnJumpButton() => jump = true;
+    public void OnJumpButtonUp() => jump = false;
 }

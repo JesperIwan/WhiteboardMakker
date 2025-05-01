e20f82d1b4bad4c5420a7aed811aaa818ea67850
laurathansky <laura.thansky@gmail.com>
2025-05-01 11:01:38 +0200

    Interface

--- Whiteboard Makker/Assets/Scripts/DestroyableLine.cs
+++ Whiteboard Makker/Assets/Scripts/DestroyableLine.cs
@@ -1,22 +1,24 @@
 using UnityEngine;
 
-public class DestroyableLine : MonoBehaviour
+public class DestroyableLine : MonoBehaviour, IDestroyable
 {
     public int hitsToDestroy = 5;
     private int hitCount = 0;
+
+    public void TakeHit() //this is from the interface. and we can implement the needed features for this object 
+    {
+        hitCount++;
+        if (hitCount >= hitsToDestroy)
+        {
+            Destroy(gameObject); // Line disappears after 5 hits w bullet
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Bullet"))
         {
-            hitCount++;
-
-            if (hitCount >= hitsToDestroy)
-            {
-                Destroy(gameObject); // Line disappears
-            }
-
-            // Don't destroy the bullet — let object pool handle it
+           TakeHit(); // Call the TakeHit method when the bullet hits the line 
         }
     }
 }

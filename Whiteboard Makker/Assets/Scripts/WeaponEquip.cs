6eea35ff5aa3488386ad6dc048bec5c37bc171d1
MasterIwan <114767161+MasterIwan@users.noreply.github.com>
2025-05-01 19:36:29 +0200

    ok

---
+++ Whiteboard Makker/Assets/Scripts/WeaponEquip.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class WeaponEquip : MonoBehaviour
+{
+    public GameObject pistol; 
+   // public GameObject hammer;
+    public string tmp;
+
+    private void Awake()
+    {
+        WeaponToEquip("na");
+    }
+    private void Update()
+    {
+        WeaponToEquip(tmp);
+    }
+    public void WeaponToEquip(string weapon)
+    {
+        if (weapon == "pistol")
+        {
+            pistol.SetActive(true); 
+            //hammer.SetActive(false);
+        }
+        else if(weapon =="hammer")
+        {
+            pistol.SetActive(false);
+            //hammer.SetActive(true);
+        }
+        else
+        {
+            pistol.SetActive(false);
+            //hammer.SetActive(false);
+        }
+
+    }
+}
